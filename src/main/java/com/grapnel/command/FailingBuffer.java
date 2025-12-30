43855c0cd81c21f8468288a017ad6f623473730c
Skrepy <703189721@qq.com>
2025-12-30 20:28:32 +0800

    ✨添加命令

---
+++ src/main/java/com/grapnel/command/FailingBuffer.java
@@ -0,0 +1,30 @@
+package com.grapnel.command;
+
+import com.grapnel.Grapnel;
+import com.mojang.brigadier.arguments.BoolArgumentType;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
+
+public class FailingBuffer {
+    public static boolean failingBuffer = true;
+
+    public static void register(RegisterCommandsEvent event) {
+        event.getDispatcher().register(Commands.literal("grapnel_settings")
+                .requires(source -> source.hasPermission(2))
+                .then(Commands.literal("enableFailingBuffer")
+                        .then(Commands.argument("enabled", BoolArgumentType.bool())
+                                .executes(context -> {
+                                    boolean enabled = BoolArgumentType.getBool(context, "enabled");
+                                    failingBuffer = enabled;
+                                    Component message = enabled ?
+                                            Component.translatable("command.grapnel.grapnel_settings.enableFailingBuffer.enable") :
+                                            Component.translatable("command.grapnel.grapnel_settings.enableFailingBuffer.disable");
+
+                                    Grapnel.LOGGER.info(message.getString());
+                                    context.getSource().sendSuccess(() -> message, false);
+
+                                    return 1;
+                                }))));
+    }
+}
